000ba57e4c7d5da8b72f07bdbaa45edfb869c561
Luis <luis.guajardo@mediamonks.com>
2023-12-07 15:58:01 +0100

    changed led pin to SPI 10 III

--- test-neopixel.py
+++ test-neopixel.py
@@ -2,7 +2,7 @@ import time
 from rpi_ws281x import PixelStrip, Color
 
 # LED strip configuration:
-LED_COUNT = 7        # Number of LED pixels
+LED_COUNT = 1        # Number of LED pixels
 LED_PIN = 10          # GPIO pin connected to the pixels (18 uses PWM)
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz
 LED_DMA = 10          # DMA channel to use for generating signal
@@ -13,24 +13,21 @@ LED_INVERT = False    # True to invert the signal
 strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
 strip.begin()
 
-try:
-    while True:
-        # Example: cycle colors
-        for i in range(strip.numPixels()):
-            strip.setPixelColor(i, Color(255, 0, 0))  # Red color
-            strip.show()
-            time.sleep(0.5)
-        for i in range(strip.numPixels()):
-            strip.setPixelColor(i, Color(0, 255, 0))  # Green color
-            strip.show()
-            time.sleep(0.5)
-        for i in range(strip.numPixels()):
-            strip.setPixelColor(i, Color(0, 0, 255))  # Blue color
-            strip.show()
-            time.sleep(0.5)
-except KeyboardInterrupt:
-    # Clear the color of all pixels to turn them off
-    for i in range(strip.numPixels()):
-        strip.setPixelColor(i, Color(0, 0, 0))
-        time.sleep(0.5)
-    strip.show()
+
+strip.setPixelColor(0, Color(255, 0, 0))  # Red color
+strip.show()
+time.sleep(1)
+
+
+strip.setPixelColor(0, Color(255, 255, 153))  # Red color
+strip.show()
+time.sleep(1)
+
+
+strip.setPixelColor(0, Color(0, 255, 0))  # Red color
+strip.show()
+time.sleep(1)
+
+strip.setPixelColor(0, Color(0, 0, 0))  # Red color
+strip.show()
+time.sleep(1)

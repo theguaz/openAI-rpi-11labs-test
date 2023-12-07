8fe7da6e3527631a2f0648a2c8e682ca0ef7dc78
Luis <luis.guajardo@mediamonks.com>
2023-12-07 18:27:21 +0100

    testing sockets IX

--- test-neopixel.py
+++ test-neopixel.py
@@ -15,9 +15,9 @@ LED_INVERT = False   # True to invert the signal (when using NPN transistor leve
 strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
 strip.begin()
 
-def pulsate(color, total_duration=0.15):
+def pulsate(color, total_duration=0.015):
     """ Gradually change the LED brightness to create a pulsating effect over a total duration """
-    steps = 10  # Number of brightness levels
+    steps = 128  # Number of brightness levels
     num_cycles = 2  # Brighten and dim count as 2 cycles
     interval = total_duration / (steps * num_cycles)
 

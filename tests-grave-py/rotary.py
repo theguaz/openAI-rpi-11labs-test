f2fdf644bd41f9b799069ea67242d558a03d8c49
Luis <luis.guajardo@mediamonks.com>
2024-05-06 12:34:44 +0200

    added rotary test

--- tests-grave-py/rotary.py
+++ tests-grave-py/rotary.py
@@ -17,24 +17,34 @@ items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"]
 current_item = 0  # Start with the first item
 
 clkLastState = GPIO.input(CLK)
+buttonPressedTime = 0
+debounceTime = 0.3  # Debounce time in seconds
 
 try:
     while True:
         clkState = GPIO.input(CLK)
         dtState = GPIO.input(DT)
+        swState = GPIO.input(SW)
+        
+        # Handle rotation
         if clkState != clkLastState:
-            if dtState != clkState:
-                current_item += 1
-            else:
-                current_item -= 1
-            current_item = current_item % len(items)  # Wrap around
-            print("Selected:", items[current_item])
+            if time.time() - buttonPressedTime > debounceTime:  # Debounce by time
+                if dtState != clkState:
+                    current_item += 1
+                else:
+                    current_item -= 1
+                current_item = current_item % len(items)  # Wrap around
+                print("Selected:", items[current_item])
+                buttonPressedTime = time.time()
         clkLastState = clkState
 
         # Handle button press
-        if not GPIO.input(SW):  # If button pressed
-            print("Button Pressed - Current selection:", items[current_item])
-            time.sleep(0.5)  # Debouncing
+        if not swState:  # If button pressed
+            if time.time() - buttonPressedTime > debounceTime:  # Debounce by time
+                print("Button Pressed - Current selection:", items[current_item])
+                buttonPressedTime = time.time()  # Update the last pressed time
+            while not GPIO.input(SW):
+                time.sleep(0.02)  # Delay to wait for the button to be released
 
         time.sleep(0.01)  # Small delay to reduce CPU usage
 

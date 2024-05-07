d7bd869ba3147bde5f10026a75f9f21da49af4d7
Luis <luis.guajardo@mediamonks.com>
2024-05-07 21:48:17 +0200

    added rotary test with voice

--- tests-grave-py/rotary_b.py
+++ tests-grave-py/rotary_b.py
@@ -10,16 +10,21 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(CLK, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(DT, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-# Initial state of the encoder
+# Initial state of the encoder and time tracking
 last_clk_state = GPIO.input(CLK)
 last_dt_state = GPIO.input(DT)
 last_time = time.time()
-debounce_interval = 0.15  # 20 milliseconds
-
+debounce_interval = 0.02  # 20 milliseconds
 counter = 0
+first_change = True  # Flag to detect the first change
+
+def critical_function():
+    # Placeholder for a critical function that must complete before updating the counter
+    print("Executing critical function...")
+    time.sleep(1)  # Simulate a delay to represent the function's execution time
 
 def get_encoder_turn():
-    global last_clk_state, last_dt_state, last_time, counter
+    global last_clk_state, last_dt_state, last_time, counter, first_change
 
     clk_state = GPIO.input(CLK)
     dt_state = GPIO.input(DT)
@@ -32,9 +37,17 @@ def get_encoder_turn():
             if clk_state != last_clk_state:
                 # Determine direction
                 if clk_state != dt_state:  # Clockwise
-                    counter += 1
+                    direction = 1
                 else:  # Counterclockwise
-                    counter -= 1
+                    direction = -1
+
+                # If this is the first change, execute the critical function
+                if first_change:
+                    critical_function()
+                    first_change = False  # Reset the flag
+
+                # Update the counter after the critical function has completed
+                counter += direction
                 print("Counter:", counter)
 
             # Update the last known states and time
@@ -46,6 +59,6 @@ def get_encoder_turn():
 try:
     while True:
         get_encoder_turn()
-        time.sleep(0.01)  # Poll at 1 ms intervals
+        time.sleep(0.001)  # Poll at 1 ms intervals
 finally:
     GPIO.cleanup()  # Clean up GPIO on exit

4cd28ddb0f30937f1857441bf0eaba1ecbbcbedf
Luis <luis.guajardo@mediamonks.com>
2024-05-07 22:20:18 +0200

    added rotary test with voice

--- tests-grave-py/rotary_b.py
+++ tests-grave-py/rotary_b.py
@@ -3,34 +3,31 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-# Set up pins
-clk = 17  # Change as necessary
-dt = 27   # Change as necessary
+# Define GPIO pins
+clk = 17
+dt = 27
 GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-# Initialize counter
-counter = 0
+# Initial state
 clkLastState = GPIO.input(clk)
 
-def rotary_callback(clk):
-    global counter, clkLastState
+def rotary_callback(channel):
+    global clkLastState
     clkState = GPIO.input(clk)
     dtState = GPIO.input(dt)
     if clkState != clkLastState:
         if dtState != clkState:
-            counter += 1
+            print("Rotated Clockwise")
         else:
-            counter -= 1
-        print("Counter: ", counter)
+            print("Rotated Counterclockwise")
     clkLastState = clkState
-    time.sleep(0.01)  # debounce time
 
-# Add event detection to the CLK pin
+# Attach event to pin
 GPIO.add_event_detect(clk, GPIO.BOTH, callback=rotary_callback, bouncetime=300)
 
 try:
     while True:
-        time.sleep(0.01)
+        time.sleep(0.1)  # Lower sleep time here for responsiveness
 except KeyboardInterrupt:
     GPIO.cleanup()

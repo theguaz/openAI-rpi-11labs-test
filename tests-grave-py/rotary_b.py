7d300abcb0852c09cde40e3474b907ad8384d2bf
Luis <luis.guajardo@mediamonks.com>
2024-05-07 22:25:49 +0200

    added rotary test with voice

--- tests-grave-py/rotary_b.py
+++ tests-grave-py/rotary_b.py
@@ -1,45 +1,68 @@
-import RPi.GPIO as GPIO
-import time
+import os
+from RPi import GPIO
+ 
 
-GPIO.setmode(GPIO.BCM)
-
-# Define GPIO pins
+ 
+step = 5 #linear steps for increasing/decreasing volume
+paused = False #paused state
+ 
+#tell to GPIO library to use logical PIN names/numbers, instead of the physical PIN numbers
+GPIO.setmode(GPIO.BCM) 
+ 
+#set up the pins we have been using
 clk = 17
 dt = 27
-GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-# Initial state
+sw = 22
+ 
+#set up the GPIO events on those pins
+GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(sw, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+ 
+#get the initial states
+counter = 0
 clkLastState = GPIO.input(clk)
-
-def rotary_callback_raising(channel):
-    global clkLastState
-    clkState = GPIO.input(clk)
-    dtState = GPIO.input(dt)
-    if clkState != clkLastState:
-        if dtState != clkState:
-            print("Rotated Clockwise")
-        else:
-            print("Rotated Counterclockwise")
-    clkLastState = clkState
-
-def rotary_callback_falling(channel):
-    global clkLastState
-    clkState = GPIO.input(clk)
-    dtState = GPIO.input(dt)
-    if clkState != clkLastState:
-        if dtState != clkState:
-            print("Rotated Clockwise")
-        else:
-            print("Rotated Counterclockwise")
-    clkLastState = clkState
-
-# Attach event to pin
-GPIO.add_event_detect(clk, GPIO.RISING, callback=rotary_callback_raising, bouncetime=100)
-GPIO.add_event_detect(clk, GPIO.FALLING, callback=rotary_callback_falling, bouncetime=100)
-
-try:
-    while True:
-        time.sleep(0.1)  # Lower sleep time here for responsiveness
-except KeyboardInterrupt:
-    GPIO.cleanup()
+dtLastState = GPIO.input(dt)
+swLastState = GPIO.input(sw)
+ 
+#define functions which will be triggered on pin state changes
+def clkClicked(channel):
+        global counter
+        global step
+ 
+        clkState = GPIO.input(clk)
+        dtState = GPIO.input(dt)
+ 
+        if clkState == 0 and dtState == 1:
+                counter = counter + step
+                print ("Counter ", counter)
+ 
+def dtClicked(channel):
+        global counter
+        global step
+ 
+        clkState = GPIO.input(clk)
+        dtState = GPIO.input(dt)
+         
+        if clkState == 1 and dtState == 0:
+                counter = counter - step
+                print ("Counter ", counter)
+ 
+def swClicked(channel):
+        global paused
+        paused = not paused
+        print ("Paused ", paused)             
+                 
+print ("Initial clk:", clkLastState)
+print ("Initial dt:", dtLastState)
+print ("Initial sw:", swLastState)
+print ("=========================================")
+ 
+#set up the interrupts
+GPIO.add_event_detect(clk, GPIO.FALLING, callback=clkClicked, bouncetime=300)
+GPIO.add_event_detect(dt, GPIO.FALLING, callback=dtClicked, bouncetime=300)
+GPIO.add_event_detect(sw, GPIO.FALLING, callback=swClicked, bouncetime=300)
+ 
+raw_input("Start monitoring input")
+ 
+GPIO.cleanup()

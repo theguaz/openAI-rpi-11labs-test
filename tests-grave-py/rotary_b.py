b1628c66ac831e940c1f682de2b52b5f77e98590
Luis <luis.guajardo@mediamonks.com>
2024-05-07 22:32:12 +0200

    added rotary test with voice

--- tests-grave-py/rotary_b.py
+++ tests-grave-py/rotary_b.py
@@ -1,68 +1,25 @@
-import os
-from RPi import GPIO
- 
+from RPi_GPIO_Rotary import rotary
+import time
 
- 
-step = 5 #linear steps for increasing/decreasing volume
-paused = False #paused state
- 
-#tell to GPIO library to use logical PIN names/numbers, instead of the physical PIN numbers
-GPIO.setmode(GPIO.BCM) 
- 
-#set up the pins we have been using
-clk = 17
-dt = 27
-sw = 22
- 
-#set up the GPIO events on those pins
-GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(sw, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
- 
-#get the initial states
-counter = 0
-clkLastState = GPIO.input(clk)
-dtLastState = GPIO.input(dt)
-swLastState = GPIO.input(sw)
- 
-#define functions which will be triggered on pin state changes
-def clkClicked(channel):
-        global counter
-        global step
- 
-        clkState = GPIO.input(clk)
-        dtState = GPIO.input(dt)
- 
-        if clkState == 0 and dtState == 1:
-                counter = counter + step
-                print ("Counter ", counter)
- 
-def dtClicked(channel):
-        global counter
-        global step
- 
-        clkState = GPIO.input(clk)
-        dtState = GPIO.input(dt)
-         
-        if clkState == 1 and dtState == 0:
-                counter = counter - step
-                print ("Counter ", counter)
- 
-def swClicked(channel):
-        global paused
-        paused = not paused
-        print ("Paused ", paused)             
-                 
-print ("Initial clk:", clkLastState)
-print ("Initial dt:", dtLastState)
-print ("Initial sw:", swLastState)
-print ("=========================================")
- 
-#set up the interrupts
-GPIO.add_event_detect(clk, GPIO.FALLING, callback=clkClicked, bouncetime=300)
-GPIO.add_event_detect(dt, GPIO.FALLING, callback=dtClicked, bouncetime=300)
-GPIO.add_event_detect(sw, GPIO.FALLING, callback=swClicked, bouncetime=300)
- 
 
- 
-GPIO.cleanup()
+def cwTurn():
+    print("CW Turn")
+
+def ccwTurn():
+    print("CCW Turn")
+
+def buttonPushed():
+    print("Button Pushed")
+
+def valueChanged(count):
+    print(count)
+
+
+## Initialise (clk, dt, sw, ticks)
+obj = rotary.Rotary(17,27,22,2)
+
+obj.register(increment=cwTurn, decrement=ccwTurn)
+obj.register(pressed=buttonPushed, onchange=valueChanged)
+obj.start()
+time.sleep(5)
+obj.stop()
